1d1ce1cd47bcf0ac7d1316ce2df4287da969b1f2
Huy <62499107+Nguyen-Nhat-Huy@users.noreply.github.com>
2021-08-07 16:35:23 +0700

    Update bài tập Ứng dụng máy tính

---
+++ module1/bai7_bien_KieuDuLieu_ToanTu/bai_tap/ung_dung_may_tinh/calculater.js
@@ -0,0 +1,27 @@
+function addition() {
+    let number1 = parseInt(document.getElementById("ip_number1").value);
+    let number2 = parseInt(document.getElementById("ip_number2").value);
+    let result = number1 + number2;
+    alert(result);
+}
+
+function subtraction() {
+    let number1 = parseInt(document.getElementById("ip_number1").value);
+    let number2 = parseInt(document.getElementById("ip_number2").value);
+    let result = number1 - number2;
+    alert(result);
+}
+
+function multiplication() {
+    let number1 = parseInt(document.getElementById("ip_number1").value);
+    let number2 = parseInt(document.getElementById("ip_number2").value);
+    let result = number1 * number2;
+    alert(result);
+}
+
+function divsion() {
+    let number1 = parseInt(document.getElementById("ip_number1").value);
+    let number2 = parseInt(document.getElementById("ip_number2").value);
+    let result = number1 / number2;
+    alert(result);
+}
