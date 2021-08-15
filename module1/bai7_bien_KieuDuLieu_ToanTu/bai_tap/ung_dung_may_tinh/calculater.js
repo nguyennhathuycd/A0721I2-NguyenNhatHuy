703798c095914ac8806bcba77ca1f24de7449bd2
Huy <62499107+Nguyen-Nhat-Huy@users.noreply.github.com>
2021-08-15 23:14:29 +0700

    SỬa bài tập review

--- module1/bai7_bien_KieuDuLieu_ToanTu/bai_tap/ung_dung_may_tinh/calculater.js
+++ module1/bai7_bien_KieuDuLieu_ToanTu/bai_tap/ung_dung_may_tinh/calculater.js
@@ -2,26 +2,26 @@ function addition() {
     let number1 = parseInt(document.getElementById("ip_number1").value);
     let number2 = parseInt(document.getElementById("ip_number2").value);
     let result = number1 + number2;
-    alert(result);
+    document.getElementById('ip_KetQua').value = result;
 }
 
 function subtraction() {
     let number1 = parseInt(document.getElementById("ip_number1").value);
     let number2 = parseInt(document.getElementById("ip_number2").value);
     let result = number1 - number2;
-    alert(result);
+    document.getElementById('ip_KetQua').value = result;
 }
 
 function multiplication() {
     let number1 = parseInt(document.getElementById("ip_number1").value);
     let number2 = parseInt(document.getElementById("ip_number2").value);
     let result = number1 * number2;
-    alert(result);
+    document.getElementById('ip_KetQua').value = result;
 }
 
 function divsion() {
     let number1 = parseInt(document.getElementById("ip_number1").value);
     let number2 = parseInt(document.getElementById("ip_number2").value);
     let result = number1 / number2;
-    alert(result);
+    document.getElementById('ip_KetQua').value = result;
 }
