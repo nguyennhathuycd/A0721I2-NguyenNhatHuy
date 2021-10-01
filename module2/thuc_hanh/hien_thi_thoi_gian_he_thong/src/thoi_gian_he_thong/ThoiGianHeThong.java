31e2c8e3c22cf4ef7c32087d44edfa05c2c09567
Huy <62499107+Nguyen-Nhat-Huy@users.noreply.github.com>
2021-10-01 20:52:52 +0700

    Update bài thực hành Java

---
+++ module2/thuc_hanh/hien_thi_thoi_gian_he_thong/src/thoi_gian_he_thong/ThoiGianHeThong.java
@@ -0,0 +1,10 @@
+package thoi_gian_he_thong;
+import java.util.Date;
+
+public class ThoiGianHeThong {
+    public static void main(String[] args) {
+        Date now = new Date();
+
+        System.out.println("Now is: " + now);
+    }
+}
