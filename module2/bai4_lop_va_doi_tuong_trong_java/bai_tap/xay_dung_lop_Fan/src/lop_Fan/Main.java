e312f9ad4767082b8c4d8ea435063e3aea4e4c42
Huy <62499107+Nguyen-Nhat-Huy@users.noreply.github.com>
2021-10-07 17:06:25 +0700

    Update bai tap bai 4

---
+++ module2/bai4_lop_va_doi_tuong_trong_java/bai_tap/xay_dung_lop_Fan/src/lop_Fan/Main.java
@@ -0,0 +1,11 @@
+package lop_Fan;
+
+public class Main {
+    public static void main(String[] args) {
+        Fan fan1 = new Fan(3, true, 10, "Yellow");
+        Fan fan2 = new Fan(2, false, 5, "Blue");
+
+        System.out.println("Fan 1: " + fan1.to_string());
+        System.out.println("Fan 1: " + fan2.to_string());
+    }
+}
