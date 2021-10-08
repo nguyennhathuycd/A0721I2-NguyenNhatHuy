7a4034f1135cb7dec34ecc84478fbccb716b3cc7
Huy <62499107+Nguyen-Nhat-Huy@users.noreply.github.com>
2021-10-08 10:48:53 +0700

    Update bai thuc hanh cua bai 5

---
+++ module2/bai5_acess_modifier_static_method_static_property/thuc_hanh/static_method/src/static_method/TestStaticMethod.java
@@ -0,0 +1,18 @@
+package static_method;
+
+public class TestStaticMethod {
+    public static void main(String[] args) {
+
+        //creating objects
+        Student s1 = new Student(111, "Hoang");
+        Student s2 = new Student(222, "Khanh");
+        Student s3 = new Student(333, "Nam");
+
+        Student.change(); //calling change method
+        
+        //calling display method
+        s1.display();
+        s2.display();
+        s3.display();
+    }
+}
