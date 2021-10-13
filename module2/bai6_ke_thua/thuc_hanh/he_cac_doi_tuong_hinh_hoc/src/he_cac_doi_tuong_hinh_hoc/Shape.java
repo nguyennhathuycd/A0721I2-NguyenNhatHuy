66006105b14fc773d533d9b7ad2be8e21b19fe7f
Huy <62499107+Nguyen-Nhat-Huy@users.noreply.github.com>
2021-10-13 21:03:33 +0700

    Update bài thực hành bài 6

---
+++ module2/bai6_ke_thua/thuc_hanh/he_cac_doi_tuong_hinh_hoc/src/he_cac_doi_tuong_hinh_hoc/Shape.java
@@ -0,0 +1,42 @@
+package he_cac_doi_tuong_hinh_hoc;
+
+public class Shape {
+    private String color;
+    private boolean filled;
+
+    public Shape() {
+        this.color = "green";
+        this.filled = true;
+    }
+
+    public Shape(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    public String toString() {
+        String temp = "";
+        if (filled) {
+            temp = "A Shape with color of " + this.color + " and filled.";
+        } else {
+            temp = "A Shape with color of " + this.color + " and not filled.";
+        }
+        return temp;
+    }
+}
---
+++ module2/bai6_ke_thua/thuc_hanh/he_cac_doi_tuong_hinh_hoc/src/he_cac_doi_tuong_hinh_hoc/ShapeTest.java
@@ -0,0 +1,31 @@
+package he_cac_doi_tuong_hinh_hoc;
+
+public class ShapeTest {
+    public static void main(String[] args) {
+//        Shape shape = new Shape();
+//        System.out.println(shape.toString());
+//        Shape shape1 = new Shape("blue", false);
+//        System.out.println(shape1.toString());
+
+//        Circle circle = new Circle();
+//        System.out.println(circle);
+//        circle = new Circle(3.5);
+//        System.out.println(circle);
+//        circle = new Circle(3.5, "indigo", false);
+//        System.out.println(circle);
+
+//        Rectangle rectangle = new Rectangle();
+//        System.out.println(rectangle);
+//        rectangle = new Rectangle(2.3, 5.8);
+//        System.out.println(rectangle);
+//        rectangle = new Rectangle(2.5, 3.8, "orange", true);
+//        System.out.println(rectangle);
+
+        Square square = new Square();
+        System.out.println(square);
+        square = new Square(2.3);
+        System.out.println(square);
+        square = new Square(5.8, "yellow", true);
+        System.out.println(square);
+    }
+}
