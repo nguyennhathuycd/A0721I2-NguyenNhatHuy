fdaa5ea829f1819cf2bc0fe6364c8a11e4029836
Huy <62499107+Nguyen-Nhat-Huy@users.noreply.github.com>
2021-11-05 21:20:10 +0700

    Update bai 12

---
+++ module2/bai12_java_collection_framework/bai_tap/su_dung_arraylist_linkedlist/src/ultils/SortProductByNameAsc.java
@@ -0,0 +1,15 @@
+package ultils;
+
+import entity.Product;
+
+import java.util.Comparator;
+
+public class SortProductByNameAsc implements Comparator<Product> {
+    @Override
+    public int compare(Product p1, Product p2) {
+        if (p2.getPrice() == p1.getPrice()) {
+            return p2.getName().compareTo(p1.getName());
+        }
+        return (int) (p2.getPrice() - p1.getPrice());
+    }
+}
