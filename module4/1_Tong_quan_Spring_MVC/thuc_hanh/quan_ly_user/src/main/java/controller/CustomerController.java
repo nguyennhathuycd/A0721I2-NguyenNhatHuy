ad818494b2a0310f2d15cf62a947e8e8c85b73b7
Nguyễn Nhật Huy <nguyennhathuy.cd@gmail.com>
2022-02-21 00:30:54 +0700

    Update bài thực hành "Quản lý User"

---
+++ module4/1_Tong_quan_Spring_MVC/thuc_hanh/quan_ly_user/src/main/java/controller/CustomerController.java
@@ -0,0 +1,24 @@
+package controller;
+
+import model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import service.CustomerService;
+
+import java.util.List;
+
+@Controller
+public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping("/customers")
+    public ModelAndView showList() {
+        ModelAndView modelAndView = new ModelAndView("index");
+        List<Customer> customers = customerService.findAll();
+        modelAndView.addObject("customers", customers);
+        return modelAndView;
+    }
+}
---
+++ module4/1_Tong_quan_Spring_MVC/thuc_hanh/quan_ly_user/src/main/java/service/CustomerServiceFactory.java
@@ -0,0 +1,14 @@
+package service;
+
+import service.impl.SimpleCustomerServiceImpl;
+
+public class CustomerServiceFactory {
+    private static CustomerService singleton;
+
+    public static synchronized CustomerService getInstance() {
+        if (singleton == null) {
+            singleton = new SimpleCustomerServiceImpl();
+        }
+        return singleton;
+    }
+}
