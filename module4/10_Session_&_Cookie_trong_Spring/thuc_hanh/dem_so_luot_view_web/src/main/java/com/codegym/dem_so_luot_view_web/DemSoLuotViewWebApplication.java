2ede566546064a9d6428f5540e859c1dc9632285
nguyennhathuycd <87653958+nguyennhathuycd@users.noreply.github.com>
2022-03-16 21:15:21 +0700

    Update bài 10 thực hành Đếm số lượt view web

---
+++ module4/10_Session_&_Cookie_trong_Spring/thuc_hanh/dem_so_luot_view_web/src/main/java/com/codegym/dem_so_luot_view_web/DemSoLuotViewWebApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.dem_so_luot_view_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemSoLuotViewWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemSoLuotViewWebApplication.class, args);
+    }
+
+}
---
+++ module4/10_Session_&_Cookie_trong_Spring/thuc_hanh/dem_so_luot_view_web/src/test/java/com/codegym/dem_so_luot_view_web/DemSoLuotViewWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.dem_so_luot_view_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemSoLuotViewWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
