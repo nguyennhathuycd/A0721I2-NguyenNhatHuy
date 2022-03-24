db9203d7be13fd74eddc1c8ac0b7ace7f1947f8f
nguyennhathuycd <87653958+nguyennhathuycd@users.noreply.github.com>
2022-03-24 20:38:29 +0700

    Update bài 14 thực hành Sử dụng Spring Security

---
+++ module4/14_Spring_security/thuc_hanh/su_dung_Spring_security/src/main/java/com/codegym/SuDungSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.codegym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuDungSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuDungSpringSecurityApplication.class, args);
+    }
+
+}
