5f513a5ff705b436c2061c7e8cf353f1ee3c0546
nguyennhathuycd <87653958+nguyennhathuycd@users.noreply.github.com>
2022-05-05 00:21:05 +0700

    Update bài tập fibonacci

---
+++ module5/2_Typescript/fibonacci/src/main.js
@@ -0,0 +1,8 @@
+function fibonacci(number) {
+    if (number <= 2)
+        return 1;
+    return fibonacci(number - 1) + fibonacci(number - 2);
+}
+var sum = 0;
+sum = fibonacci(3) + fibonacci(5) + fibonacci(7);
+console.log("Tong cac so fibonacci la: " + sum);
